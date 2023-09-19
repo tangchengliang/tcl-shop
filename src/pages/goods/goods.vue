e746fe1e2929914e9244d43cd0ab2b5c58777c55
Sy <2548446071@qq.com>
2023-09-19 22:01:28 +0800

    弹出层组件的使用

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -38,6 +38,12 @@ const onTapImage = (url: string) => {
     urls: goods.value!.mainPictures,
   })
 }
+
+// 弹窗
+const popup = ref<{
+  open: (type?: UniHelper.UniPopupType) => void
+  close: () => void
+}>()
 </script>
 
 <template>
@@ -78,7 +84,7 @@ const onTapImage = (url: string) => {
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="popup?.open('bottom')">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -148,6 +154,13 @@ const onTapImage = (url: string) => {
       <view class="buynow"> 立即购买 </view>
     </view>
   </view>
+
+  <!-- 弹窗操作-->
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <text>test1</text>
+    <text>test2</text>
+    <button @tap="popup?.close()">关闭</button>
+  </uni-popup>
 </template>
 
 <style lang="scss">
